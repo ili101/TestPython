8d02f10edb626d7968d4716b33dea3166dc84785
ili101 <zoby101@gmail.com>
2022-01-11 15:22:57 +0200

    sub

---
+++ poetry-sub/poetry_sub/my_fun.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+class MyFun():
+    a = np.arange(15).reshape(3, 5)
+    print(a)
---
+++ poetry-sub/tests/test_my_fun.py
@@ -0,0 +1,9 @@
+import unittest
+from poetry_sub.my_fun import MyFun
+
+class TestMyFun(unittest.TestCase):
+    MyFun()
+
+
+if __name__ == '__main__':
+    unittest.main()
